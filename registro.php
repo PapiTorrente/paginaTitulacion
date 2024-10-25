327d711aa6f96dc77fbe855d32867744dd260209
ManuelLopezParedes <manuel.eduardo.lopeza@gmail.com>
2024-10-24 22:55:44 -0600

    actualizacion del formulario

--- registro.php
+++ registro.php
@@ -10,63 +10,87 @@
 
     <div class="Formulario" >
         <form action="./controlador/enviarRegistro.php" method="post">
+          <hr>
+          <div>
+                    <div class="row">
+                        <div class="input-field col s4">
+                        <i class="material-icons prefix">account_circle</i>
+                            <input type="text" id="nombre" class="validate" required maxlength="100" pattern="[a-zA-Z ]+">
+                            <label for="nombre">Nombre(s)</label>
+                            <span class="helper-text" data-error="solo letras"></span>
+                        </div>
 
-  <div class="row">
-    <div class="input-field col s6">
-      <input type="text" class="validate" name="nombre_usuario" required maxlength="100" placeholder="Ingresa tu Nombre de Usuario">
-      <label class="active" for="nombre_usuario">Nombre de Usuario: </label>
-    </div>
-  </div>
+                        <div class="input-field col s4">
+                            <input type="text" id="primer_apellido" class="validate" required maxlength="100" pattern="[a-zA-Z ]+">
+                            <label for="primer_apellido">Primer apellido</label>
+                            <span class="helper-text" data-error="solo letras"></span>
+                        </div>
 
-  <div class="row">
-    <div class="input-field col s6">
-      <input type="text" class="validate" name="carrera" required maxlength="100" placeholder="Ingresa tu Carrera">
-      <label class="active" for="carrera">Carrera: </label>
-    </div>
-  </div>
+                        <div class="input-field col s4">
+                            <input type="text" id="segundo_apellido" class="validate" required maxlength="100" pattern="[a-zA-Z ]+">
+                            <label for="segundo_apellido">Segundo apellido</label>
+                            <span class="helper-text" data-error="solo letras"></span>
+                        </div>
+                    </div>
 
-      <div class="row">
-        <div class="input-field col s12">
-          <input id="email" type="email" class="validate" nam="email" required maxlength="100" placeholder="Ingresa tu Correo">
-          <label for="email">Correo: </label>
-        </div>
-      </div>
+                    <div class="row">
+                        <div class="input-field col s6">
+                            <i class="material-icons prefix">class</i>
+                            <input type="text" id="carrera" class="validate" required maxlength="100" pattern="[a-zA-Z ]+">
+                            <label for="carrera">Carrera</label>
+                            <span class="helper-text" data-error="solo letras"></span>
+                        </div>
 
-  <div class="row">
-    <div class="input-field col s6">
-      <input type="text" class="validate" name="no_cuenta" required maxlength="9" placeholder="Ingresa tu Número de Cuenta">
-      <label class="active" for="no_cuenta">No de Cuenta: </label>
-    </div>
-  </div>
+                        <div class="input-field col s6">
+                            <input type="text" id="no_cuenta" class="validate" required maxlenght="9" pattern="\d{1,9}">
+                            <label for="no_cuenta">NÃºmero de cuenta</label>
+                            <span class="helper-text" data-error="maximo de 9 numeros sin guion"></span>
+                        </div>
+                    </div>
 
-  <div class="row">
-    <div class="input-field col s6">
-      <input type="text" class="validate" name="direccion" required maxlength="100" placeholder="Ingresa tu Dirección Particular">
-      <label class="active" for="no_cuenta">Dirección Particular: </label>
-    </div>
-  </div>
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <i class="material-icons prefix">email</i>
+                            <input type="email" id="correo" class="validate" required maxlength="100">
+                            <label for="correo">Correo Institucional</label>
+                        </div>
+                    </div>
 
-  <div class="row">
-    <div class="input-field col s6">
-      <input type="text" class="validate" name="telefono" required maxlength="12" placeholder="Ingresa tu Teléfono">
-      <label class="active" for="telefono">Teléfono: </label>
-    </div>
-  </div>
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <i class="material-icons prefix">call</i>
+                            <input type="text" id="telefono" class="validate" required maxlength="10" pattern="\d{1,9}">
+                            <label for="telefono">TelÃ©fono particular</label>
+                            <span class="helper-text" data-error="maximo de 10 numeros"></span>
+                        </div>
+                    </div>
 
-      <div class="row">
-        <div class="input-field col s12">
-          <input id="password" type="password" class="validate" name="password" require maxlength="8" placeholder="Ingresa tu Contraseña">
-          <label for="password">Contraseña: </label>
-        </div>
-      </div>
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <i class="material-icons prefix">home</i>
+                            <input type="text" id="direccion" class="validate" required maxlength="100">
+                            <label for="direccion">DirecciÃ³n particular</label>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <i class="material-icons prefix">visibility_off</i>
+                            <input type="password" id="password" class="validate" required maxlength="8">
+                            <label for="password">ContraseÃ±a</label>
+                        </div>
+                    </div>
+
+                <br><br>
+            </div>
 
              <button type="submit" name="submit">Enviar registro</button>
         </form>
     </div>
-<a href='Registro.php'>Nuevo registro</a>
-<a href='index.php'>Ver registro</a>
+    <a href='Registro.php'>Nuevo registro</a>
+    <a href='index.php'>Ver registro</a>
 
-</div>
+  </div>
 </div>
 <?php
   include ("./footer.php");
