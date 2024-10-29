53ab6186b27cb69625cbccf82446c28738887fc3
LeoGAR55 <leonardogaribaydiaz@gmail.com>
2024-10-29 13:41:15 -0600

    Modalidades plan 2119

--- index.php
+++ index.php
@@ -1,136 +1,167 @@
 <?php
-  include ("./header.php");
+include("./header.php");
 ?>
-    <div class="row">
-        <div class="col s12">
-          <ul class="tabs">
-            <li class="tab col s3"><a class="active" href="#plan2119">Plan 2119</a></li>
-            <li class="tab col s3"><a href="#plan1279">Plan 1279</a></li>
-            <li class="tab col s3"><a href="#plan0082">Plan 0082</a></li>
-            <li class="tab col s3"><a href="#plan0080">Plan 0080</a></li>
-          </ul>
-        </div>
-        
-        <div id="plan2119" class="col s12"> <!-- Primer Tarjeta prueba-->
+<div class="row">
+  <div class="col s12">
+    <ul class="tabs">
+      <li class="tab col s3"><a class="active" href="#plan2119">Plan 2119</a></li>
+      <li class="tab col s3"><a href="#plan1279">Plan 1279</a></li>
+      <li class="tab col s3"><a href="#plan0082">Plan 0082</a></li>
+      <li class="tab col s3"><a href="#plan0080">Plan 0080</a></li>
+    </ul>
+  </div>
 
-          <div style="margin: 8px; background-color: #293e6b; width: 40%;">
-            <p style="font-size: 20px; margin: 8px; color: #fff;">Requisitos establecidos en el Plan de Estudios y en las Reglas de Operación</p>
-            <ul>
-              <li><a href="./pdf/certificado-de-estudios.pdf" class="requisitos">Certificado de estudios</a></li>
-              <li><a href="./pdf/carta-liberacion-servicio.pdf" class="requisitos">Carta de liberación del servicio social</a></li>
-              <li><a href="./pdf/constancia-de-idiomas.pdf" class="requisitos">Constancia de idioma</a></li>
-              <li><a href="./pdf/actividad-formacion-complementaria.pdf" class="requisitos">Actividad de formación complementaria</a></li>
-            </ul>
-            <div style="padding: 0px 0px 8px 0px;">
-              <ul class="collapsible popout">
-                <p style="font-size: 20px; margin: 8px; color: #fff;">Modalidad: Con trabajo escrito y réplica oral</p>
-                <li>
-                  <div class="collapsible-header light-blue darken-4"><i class="material-icons">insert_drive_file</i>Titulación mediante tesis o tesina y examen profesional</div>
-                  <div class="collapsible-body"><span>----</span></div>
-                </li>
-                <li>
-                  <div class="collapsible-header light-blue darken-4"><i class="material-icons">insert_drive_file</i>Titulación por actividad de investigación en su alternativa de tesis o tesina</div>
-                  <div class="collapsible-body"><span>----</span></div>
-                </li>
-                <p style="font-size: 20px; margin: 8px; color: #fff;">Modalidad: Con trabajo escrito, sin réplica oral</p>
-                <li>
-                  <div class="collapsible-header light-blue darken-4"><i class="material-icons">insert_drive_file</i>Titulación por actividad de apoyo a la docencia</div>
-                  <div class="collapsible-body"><span>----</span></div>
-                </li>
-                <li>
-                  <div class="collapsible-header light-blue darken-4"><i class="material-icons">insert_drive_file</i>Titulación por trabajo profesional</div>
-                  <div class="collapsible-body"><span>--- </span></div>
-                </li>
-              </ul>
-            </div>
-          </div>
+  <div id="plan2119" class="col s12"> <!-- Primer Tarjeta prueba-->
 
-        </div>
-
-        <div id="plan1279" class="col s12"> <!-- Segunda Tarjeta -->
-
-        <div style="margin: 8px; background-color: #293e6b; width: 40%;">
-          <p style="font-size: 30px; margin: 8px; color: #fff;">Requisitos</p>
-            <div style="padding: 0px 0px 8px 0px;">
-              <ul class="collapsible popout">
-                <li>
-                  <div class="collapsible-header light-blue darken-4"><i class="material-icons">insert_drive_file</i></div>
-                  <div class="collapsible-body"><span></span></div>
-                </li>
-                <li>
-                  <div class="collapsible-header light-blue darken-4"><i class="material-icons">insert_drive_file</i></div>
-                  <div class="collapsible-body"><span>.</span></div>
-                </li>
-                <li>
-                  <div class="collapsible-header light-blue darken-4"><i class="material-icons">insert_drive_file</i></div>
-                  <div class="collapsible-body"><span></span></div>
-                </li>
-                <li>
-                  <div class="collapsible-header light-blue darken-4"><i class="material-icons">insert_drive_file</i></div>
-                  <div class="collapsible-body"><span> </span></div>
-                </li>
-              </ul>
-            </div>
-          </div>
-
-        </div>
-
-        <div id="plan0082" class="col s12"> <!-- Tercera Tarjeta -->
-
-        <div style="margin: 8px; background-color: #293e6b; width: 40%;">
-            <p style="font-size: 30px; margin: 8px; color: #fff;">Requisitos</p>
-            <div style="padding: 0px 0px 8px 0px;">
-              <ul class="collapsible popout">
-                <li>
-                  <div class="collapsible-header light-blue darken-4"><i class="material-icons">insert_drive_file</i></div>
-                  <div class="collapsible-body"><span></span></div>
-                </li>
-                <li>
-                  <div class="collapsible-header light-blue darken-4"><i class="material-icons">insert_drive_file</i></div>
-                  <div class="collapsible-body"><span></span></div>
-                </li>
-                <li>
-                  <div class="collapsible-header light-blue darken-4"><i class="material-icons">insert_drive_file</i></div>
-                  <div class="collapsible-body"><span></span></div>
-                </li>
-                <li>
-                  <div class="collapsible-header light-blue darken-4"><i class="material-icons">insert_drive_file</i></div>
-                  <div class="collapsible-body"><span> </span></div>
-                </li>
-              </ul>
-            </div>
-          </div>
-
-        </div>
-
-        <div id="plan0080" class="col s12"> <!-- Cuarta Tarjeta -->
-
-        <div style="margin: 8px; background-color: #293e6b; width: 40%;">
-            <p style="font-size: 30px; margin: 8px; color: #fff;">Requisitos</p>
-            <div style="padding: 0px 0px 8px 0px;">
-              <ul class="collapsible popout">
-                <li>
-                  <div class="collapsible-header light-blue darken-4"><i class="material-icons">insert_drive_file</i></div>
-                  <div class="collapsible-body"><span></span></div>
-                </li>
-                <li>
-                  <div class="collapsible-header light-blue darken-4"><i class="material-icons">insert_drive_file</i></div>
-                  <div class="collapsible-body"><span></span></div>
-                </li>
-                <li>
-                  <div class="collapsible-header light-blue darken-4"><i class="material-icons">insert_drive_file</i></div>
-                  <div class="collapsible-body"><span></span></div>
-                </li>
-                <li>
-                  <div class="collapsible-header light-blue darken-4"><i class="material-icons">insert_drive_file</i></div>
-                  <div class="collapsible-body"><span> </span></div>
-                </li>
-              </ul>
-            </div>
-          </div>
-
-        </div>
+    <div style="margin: 8px; background-color: #293e6b; width: 99%;">
+      <p style="font-size: 20px; margin: 8px; color: #fff;">Requisitos establecidos en el Plan de Estudios y en las Reglas de Operación</p>
+      <ul class="custom-list">
+        <li><a href="./pdf/certificado-de-estudios.pdf" class="requisitos">Certificado de estudios</a></li>
+        <li><a href="./pdf/carta-liberacion-servicio.pdf" class="requisitos">Carta de liberación del servicio social</a></li>
+        <li><a href="./pdf/constancia-de-idiomas.pdf" class="requisitos">Constancia de idioma</a></li>
+        <li><a href="./pdf/actividad-formacion-complementaria.pdf" class="requisitos">Actividad de formación complementaria</a></li>
+      </ul>
+      <div style="padding: 0px 0px 8px 0px;">
+        <ul class="collapsible popout">
+          <p style="font-size: 20px; margin: 8px; color: #fff;">Modalidad: Con trabajo escrito y réplica oral</p>
+          <li>
+            <div class="collapsible-header light-blue darken-4"><i class="material-icons">insert_drive_file</i>Titulación mediante tesis o tesina y examen profesional</div>
+            <div class="collapsible-body"><span>De conformidad con los artículos 32, 33 y 34 de los LITL FES Aragón, esta opción de titulación comprenderá una tesis individual o grupal o una tesina individual, y su réplica oral. 
+            En el caso de tesis grupal, podrá conformarse por tres integrantes, pueden ser del mismo programa o de diferentes programas. La evaluación y la toma de protesta de esta opción se realizarán conforme a los artículos 17, 18, 22, 23, 24, 25, 
+            26, 27 y 30 de los LITL FES Aragón Se podrá un máximo de tres integrantes para la elaboración de tesis grupal, previa valoración del proyecto, justificación de los integrantes y aprobación de la Jefatura de Carrera.</span></div>
+          </li>
+          <li>
+            <div class="collapsible-header light-blue darken-4"><i class="material-icons">insert_drive_file</i>Titulación por actividad de investigación en su alternativa de tesis o tesina</div>
+            <div class="collapsible-body"><span>----</span></div>
+          </li>
+          <p style="font-size: 20px; margin: 8px; color: #fff;">Modalidad: Con trabajo escrito, sin réplica oral</p>
+          <li>
+            <div class="collapsible-header light-blue darken-4"><i class="material-icons">insert_drive_file</i>Titulación por actividad de apoyo a la docencia</div>
+            <div class="collapsible-body"><span>----</span></div>
+          </li>
+          <li>
+            <div class="collapsible-header light-blue darken-4"><i class="material-icons">insert_drive_file</i>Titulación por trabajo profesional</div>
+            <div class="collapsible-body"><span>--- </span></div>
+          </li>
+          <li>
+            <div class="collapsible-header light-blue darken-4"><i class="material-icons">insert_drive_file</i>Titulación por servicio social</div>
+            <div class="collapsible-body"><span>--- </span></div>
+          </li>
+          <li>
+            <div class="collapsible-header light-blue darken-4"><i class="material-icons">insert_drive_file</i>Titulación por actividad de investigación en su alternativa de artículo académico</div>
+            <div class="collapsible-body"><span>--- </span></div>
+          </li>
+          <p style="font-size: 20px; margin: 8px; color: #fff;">Modalidad: Sin trabajo escrito</p>
+          <li>
+            <div class="collapsible-header light-blue darken-4"><i class="material-icons">insert_drive_file</i>Titulación mediante examen general de conocimientos externo</div>
+            <div class="collapsible-body"><span>--- </span></div>
+          </li>
+          <li>
+            <div class="collapsible-header light-blue darken-4"><i class="material-icons">insert_drive_file</i>Titulación por totalidad de créditos y alto nivel académico</div>
+            <div class="collapsible-body"><span>--- </span></div>
+          </li>
+          <li>
+            <div class="collapsible-header light-blue darken-4"><i class="material-icons">insert_drive_file</i>Titulación mediante estudios en posgrado</div>
+            <div class="collapsible-body"><span>--- </span></div>
+          </li>
+          <li>
+            <div class="collapsible-header light-blue darken-4"><i class="material-icons">insert_drive_file</i>Titulación por ampliación y profundización de conocimientos en su alternativa de semestre adicional</div>
+            <div class="collapsible-body"><span>--- </span></div>
+          </li>
+          <li>
+            <div class="collapsible-header light-blue darken-4"><i class="material-icons">insert_drive_file</i>Titulación por ampliación y profundización de conocimientos en su alternativa de cursos o diplomados de educación continua</div>
+            <div class="collapsible-body"><span>--- </span></div>
+          </li>
+        </ul>
+      </div>
     </div>
+
+  </div>
+
+  <div id="plan1279" class="col s12"> <!-- Segunda Tarjeta -->
+
+    <div style="margin: 8px; background-color: #293e6b; width: 40%;">
+      <p style="font-size: 30px; margin: 8px; color: #fff;">Requisitos</p>
+      <div style="padding: 0px 0px 8px 0px;">
+        <ul class="collapsible popout">
+          <li>
+            <div class="collapsible-header light-blue darken-4"><i class="material-icons">insert_drive_file</i></div>
+            <div class="collapsible-body"><span></span></div>
+          </li>
+          <li>
+            <div class="collapsible-header light-blue darken-4"><i class="material-icons">insert_drive_file</i></div>
+            <div class="collapsible-body"><span>.</span></div>
+          </li>
+          <li>
+            <div class="collapsible-header light-blue darken-4"><i class="material-icons">insert_drive_file</i></div>
+            <div class="collapsible-body"><span></span></div>
+          </li>
+          <li>
+            <div class="collapsible-header light-blue darken-4"><i class="material-icons">insert_drive_file</i></div>
+            <div class="collapsible-body"><span> </span></div>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+  </div>
+
+  <div id="plan0082" class="col s12"> <!-- Tercera Tarjeta -->
+
+    <div style="margin: 8px; background-color: #293e6b; width: 40%;">
+      <p style="font-size: 30px; margin: 8px; color: #fff;">Requisitos</p>
+      <div style="padding: 0px 0px 8px 0px;">
+        <ul class="collapsible popout">
+          <li>
+            <div class="collapsible-header light-blue darken-4"><i class="material-icons">insert_drive_file</i></div>
+            <div class="collapsible-body"><span></span></div>
+          </li>
+          <li>
+            <div class="collapsible-header light-blue darken-4"><i class="material-icons">insert_drive_file</i></div>
+            <div class="collapsible-body"><span></span></div>
+          </li>
+          <li>
+            <div class="collapsible-header light-blue darken-4"><i class="material-icons">insert_drive_file</i></div>
+            <div class="collapsible-body"><span></span></div>
+          </li>
+          <li>
+            <div class="collapsible-header light-blue darken-4"><i class="material-icons">insert_drive_file</i></div>
+            <div class="collapsible-body"><span> </span></div>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+  </div>
+
+  <div id="plan0080" class="col s12"> <!-- Cuarta Tarjeta -->
+
+    <div style="margin: 8px; background-color: #293e6b; width: 40%;">
+      <p style="font-size: 30px; margin: 8px; color: #fff;">Requisitos</p>
+      <div style="padding: 0px 0px 8px 0px;">
+        <ul class="collapsible popout">
+          <li>
+            <div class="collapsible-header light-blue darken-4"><i class="material-icons">insert_drive_file</i></div>
+            <div class="collapsible-body"><span></span></div>
+          </li>
+          <li>
+            <div class="collapsible-header light-blue darken-4"><i class="material-icons">insert_drive_file</i></div>
+            <div class="collapsible-body"><span></span></div>
+          </li>
+          <li>
+            <div class="collapsible-header light-blue darken-4"><i class="material-icons">insert_drive_file</i></div>
+            <div class="collapsible-body"><span></span></div>
+          </li>
+          <li>
+            <div class="collapsible-header light-blue darken-4"><i class="material-icons">insert_drive_file</i></div>
+            <div class="collapsible-body"><span> </span></div>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+  </div>
+</div>
 <?php
-  include ("./footer.php");
+include("./footer.php");
 ?>
